40c7a89d6d9f76a9f9ad021ebad7e25c91d50ce4
Bo-Eamonn <bo.eamonn@gmail.com>
2021-02-09 00:56:08 +0100

    Bo | Updated login to fit project

--- templates/login/login.php
+++ templates/login/login.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>HealthOne | Inloggen</title>
+    <title>Gameshop | Inloggen</title>
+    <link rel="stylesheet" href="/gameshop/assets/css/normaliz.css">
     <link rel="stylesheet" href="/gameshop/assets/css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
 </head>
